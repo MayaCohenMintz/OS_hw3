829c5607e5f2f1dad75547093c94d1495905b52c
MayaCohenMintz <114333604+MayaCohenMintz@users.noreply.github.com>
2024-07-11 17:55:30 +0000

    made changes

--- message_slot.c
+++ message_slot.c
@@ -8,15 +8,20 @@
 #include <linux/fs.h>       /* for register_chrdev */
 #include <linux/uaccess.h>  /* for get_user and put_user */
 #include <linux/string.h>   /* for memset. NOTE - not string.h!*/
+# include <linux/slab.h>
 #include "message_slot.h"
+#include "errno.h"
 
 MODULE_LICENSE("GPL");
 
 static ch_node* devices_array [256] = {NULL}; // ith cell represents head of LL of that message slot's channels. 
 // so each cell contains either NULL or the head of a LL of channel_nodes. 
 
-//================== HELPER FUNCTIONS FOR CH_NODE LINKED LISTS ===========================
-// search for channel_id in a LL of ch_nodes
+
+ch_node* get_channel_ptr(unsigned long channel_id, ch_node* phead); // returns pointer to the channel 
+int create_and_append(unsigned long channel_id, ch_node* phead); // create new ch_node with the channel_id and
+// append to end of LL. Returns 0 on success and -1 on failure
+
 // append ch_node to LL (receives head of that LL)
 // free channel (that specific channel only? shouldn't there be something recursive here since it
 // is a LL?)
@@ -26,7 +31,56 @@ static ssize_t device_read(struct file* file, char __user* buffer,size_t length,
 static ssize_t device_write(struct file* file, const char __user* buffer, size_t length, loff_t* offset);
 static long device_ioctl(struct file* file, unsigned int ioctl_command_id, unsigned long ioctl_param);
 
-//================== DEVICE FUNCTIONS ===========================
+//================== HELPER FUNCTIONS IMPLEMENTATION ===========================
+
+ch_node* get_channel_ptr(unsigned long channel_id, ch_node* phead)
+{
+    ch_node* pcurr = phead;
+    while(pcurr != NULL)
+    {
+        if(pcurr -> id == channel_id)
+        {
+            return pcurr;
+        }
+        pcurr = pcurr.next;
+    }
+    return NULL;
+}
+
+int create_and_append(unsigned long channel_id, ch_node* phead)
+{
+    int status;
+    ch_node new_channel;
+    ch_node* pcurr = phead;
+
+    // trying to allocate memory for a new ch_node. On error kmalloc returns NULL and sets errno
+    &new_channel = kmalloc(sizeof(ch_node), GFP_KERNEL);
+    if(&new_channel == NULL)
+    {
+        printk(KERN_ERR "Failed to allocate memory for new channel\n");
+        return -1;
+    }
+
+    // Creating new ch_node
+    new_channel.id = channel_id;
+    new_channel.next = NULL;
+    new_channel.msg_len = 0;
+    new_channel.msg = "";
+
+    // Getting to last node of LL 
+    while(pcurr.next != NULL)
+    {
+        pcurr = pcurr.next;
+    }
+    // Now pcurr is a pointer to the last ch_node in the LL
+    pcurr.next = &new_channel;
+    // Returning pointer to newly created ch_node
+    return &new_channel; 
+}
+
+
+
+//================== DEVICE FUNCTIONS IMPLEMENTATION ===========================
 static int device_open(struct inode* inode, struct file* file)
 {
     // ensures that the ch_node pointer of this message_slot device is instantiated.
@@ -63,17 +117,41 @@ static ssize_t device_write(struct file* file, const char __user* buffer, size_t
     return 0;
 }
  
-static long device_ioctl(struct file* file, unsigned int ioctl_command, unsigned long ioctl_param)
+static long device_ioctl(struct file* file, unsigned int ioctl_command, unsigned long channel_id)
 {
-    switch(ioctl_command) 
+    // What this method should do: invoking the ioctl() sets the file descriptor’s (of the message_slot device) channel id. 
+    // Subsequent reads/writes on this file descriptor (i.e. this message_slot) will receive/send
+    // messages on the specified channel.
+
+    int minor_num;
+    ch_node* phead;
+
+    // Validate input
+    if(ioctl_command != MESSAGE_SLOT_CHANNEL)
     {
-        case MESSAGE_SLOT_CHANNEL:
-            printk(KERN_INFO "message slot: ioctl command MESSAGE_SLOT_CHANNEL received with param: %lu\n", ioctl_param);
-            // Handle the command here, e.g., set the channel number
-            break;
-        default:
-            return -EINVAL; // Invalid command
+        printk(KERN_INFO "Invalid ioctl command: command was %lu and not MESSAGE_SLOT_CHANNEL\n", ioctl_param);
+        errno = EINVAL; 
+        return -1;
     }
+    else if (channel_id == 0)
+    {
+        printk(KERN_INFO "Invalid channel id");
+        errno = EINVAL; 
+        return -1;
+    }
+
+    // Store the channel id to be associated with the message_slot device that invoked the ioctl:
+    file -> private_data = (void*)channel_id; 
+    // Check if the channel is in the linked list, if not create a ch_node for it
+    minor_num = iminor(file_inode(file)); // get minor number of the message_slot device that invoked the ioctl
+    phead = devices_array[minor_num];
+    if(get_channel_ptr(channel_id, phead) == NULL)
+    {
+        create_and_append(channel_id, phead);
+    }
+
+
+    
     return 0;
 }
 
