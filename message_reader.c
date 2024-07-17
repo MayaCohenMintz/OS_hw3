8af878d598bc4e256879875e07b11c2e84ff0259
MayaCohenMintz <cohenmintz@mail.tau.ac.il>
2024-07-17 20:19:11 +0300

    Passes python!

--- message_reader.c
+++ message_reader.c
@@ -26,12 +26,12 @@ int main(int argc, char* argv[])
     file_path = argv[1];
     channel_id = (unsigned long)atoi(argv[2]);
 
-    printf("message_reader: argv[2] is %s, channel id is %lu. Are they the same?\n", argv[2], channel_id);
+    //printf("message_reader: argv[2] is %s, channel id is %lu. Are they the same?\n", argv[2], channel_id);
 
     // 1. Open specified message slot device file
-    printf("message_reader: Now trying to open file:\n");
+    //printf("message_reader: Now trying to open file:\n");
     msg_slot_fd = open(file_path, O_RDWR);
-    printf("message_reader: msg_slot_fd: %i\n", msg_slot_fd);
+    //printf("message_reader: msg_slot_fd: %i\n", msg_slot_fd);
     if(msg_slot_fd < 0)
     {
         perror("Error in opening device file\n");
@@ -39,18 +39,18 @@ int main(int argc, char* argv[])
     }
 
     // 2. Set the channel id to the id specified on the command line
-    printf("message_reader: trying to invoke ioctl on msg_slot_fd = %i and channel id = %lu\n", msg_slot_fd, channel_id);
+    //printf("message_reader: trying to invoke ioctl on msg_slot_fd = %i and channel id = %lu\n", msg_slot_fd, channel_id);
     if (ioctl(msg_slot_fd, MSG_SLOT_CHANNEL, channel_id) == -1) 
     {
         perror("message_reader: Error in setting channel\n");
         exit(1); 
     }
-    printf("message_reader: Channel set successfully with id %lu\n", channel_id);
+    //printf("message_reader: Channel set successfully with id %lu\n", channel_id);
     // 3. Read a message from the message slot file to a buffer. 
     // (reading up to BUF_LEN bytes, so no need to know length of actual message)
-    printf("message_reader: Trying to read message from buffer: \n");
+    //printf("message_reader: Trying to read message from buffer: \n");
     num_bytes_read = read(msg_slot_fd, buffer, BUF_LEN);
-    printf("num of bytes read is %i\n", num_bytes_read);
+    //printf("num of bytes read is %i\n", num_bytes_read);
     if(num_bytes_read == -1)
     {
         perror("message_reader: Error in reading from channel\n");
@@ -58,13 +58,13 @@ int main(int argc, char* argv[])
     }
     
     // 4. Close the device.
-    printf("message_reader: closing device\n");
+    //printf("message_reader: closing device\n");
     close(msg_slot_fd);
     // 5. Print the message to standard output (using the write() system call). Print only the message,
     // without any additional text
-    printf("message_reader: printing to std outpud: \n");
+    //printf("message_reader: printing to std outpud: \n");
     num_bytes_written = write(STDOUT_FILENO, buffer, num_bytes_read);
-    printf("\nnum of bytes written to output is %i", num_bytes_written);
+    //printf("\nnum of bytes written to output is %i", num_bytes_written);
     if(num_bytes_written == -1)
     {
         perror("message_reader: Error in writing message to std output\n");
